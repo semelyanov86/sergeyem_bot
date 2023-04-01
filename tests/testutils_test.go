83c464c2595b71bf314939e266db334554f51b33
Sergey <sergey86@gmail.com>
2023-04-01 14:59:55 +0200

    Added test utils

--- tests/testutils_test.go
+++ tests/testutils_test.go
@@ -1,1 +1,45 @@
 package tests
+
+import (
+	"database/sql"
+	"os"
+	"testing"
+)
+
+func NewTestDB(t *testing.T) (*sql.DB, func()) {
+	var dbCred = os.Getenv("BOT_TEST_DB")
+	db, err := sql.Open("mysql", dbCred)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	migrations := [...]string{
+		"../migrations/000001_create_settings_table.up.sql",
+	}
+	for _, migration := range migrations {
+		script, err := os.ReadFile(migration)
+		if err != nil {
+			t.Fatal(err)
+		}
+		_, err = db.Exec(string(script))
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+	return db, func() {
+		migrations := [...]string{
+			"../../migrations/000001_create_settings_table.down.sql",
+		}
+		for _, migration := range migrations {
+			script, err := os.ReadFile(migration)
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, err = db.Exec(string(script))
+			if err != nil {
+				t.Fatal(err)
+			}
+		}
+		db.Close()
+	}
+}
